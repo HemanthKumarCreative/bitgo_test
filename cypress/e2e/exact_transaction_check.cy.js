348a6469a0dacf854c9dd337754d5bb4ef672c83
Hemanth Kumar <avisihks@gmail.com>
2023-09-12 11:00:25 +0530

    Update exact transaction check

--- cypress/e2e/exact_transaction_check.cy.js
+++ cypress/e2e/exact_transaction_check.cy.js
@@ -1,16 +1,26 @@
+const URL =
+  "https://blockstream.info/block/000000000000000000076c036ff5119e5a5a74df77abf64203473364509f7732";
 describe("Scrape Transactions", () => {
   it("Visits the website and scrapes transactions", () => {
-    cy.visit(
-      "https://blockstream.info/block/000000000000000000076c036ff5119e5a5a74df77abf64203473364509f7732"
-    );
+    cy.visit(URL);
     cy.contains("25 of 2875 Transactions")
       .parent()
       .then(($parentElement) => {
-        const parentTag = $parentElement[0].tagName.toLowerCase();
-        const parentClass = $parentElement.attr("class");
+        const $divElements = $parentElement.find(".transaction-box");
 
-        cy.log(`Parent Element Tag Name: ${parentTag}`);
-        cy.log(`Parent Element Class: ${parentClass}`);
+        $divElements.each((index, element) => {
+          const $vins = Cypress.$(element).find(".vin");
+          const $vouts = Cypress.$(element).find(".vout");
+
+          const vinsCount = $vins?.length;
+          const voutsCount = $vouts?.length;
+          if (voutsCount === 2 && vinsCount === 1) {
+            const $txn = Cypress.$(element).find(".txn");
+            cy.log(
+              `The transaction hash with 1 Input and 2 output is ${$txn.text()}`
+            );
+          }
+        });
       });
   });
 });
